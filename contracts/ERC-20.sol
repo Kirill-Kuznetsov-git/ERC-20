19a08232ee4acbf22dbfea44cbdf988da3feef70
Kirill-Kuznetsov-git <kirill.kuznetsov2002@yandex.ru>
2022-05-13 19:39:35 +0300

    add interface

--- contracts/ERC-20.sol
+++ contracts/ERC-20.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ERC20 {
+import "./ERCinterface.sol";
+
+contract ERC20 is ERCInterface {
     address owner;
     mapping(address => uint256) private _balances;
     mapping(address => mapping(address => uint256)) private _allowances;
@@ -9,9 +11,6 @@ contract ERC20 {
     string private _name;
     string private _symbol;
     uint256 private _totalTokens;
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint value);
 
     modifier enoughTokens(address from, uint256 value){
         require(balanceOf(from) >= value, 'not enough tokens');
@@ -30,52 +29,52 @@ contract ERC20 {
         mint(account, initialSupply);
     }
 
-    function name() public view returns(string memory){
+    function name() override public view returns(string memory){
         return _name;
     }
-    function symbol() public view returns(string memory){
+    function symbol() override public view returns(string memory){
         return _symbol;
     }
-    function decimals() public pure returns(uint8){
+    function decimals() override public pure returns(uint8){
         return DECIMALS;
     }
-    function totalSupply() public view returns(uint256){
+    function totalSupply() override public view returns(uint256){
         return _totalTokens;
     }
 
-    function balanceOf(address ownerTokens) public view returns(uint256){
+    function balanceOf(address ownerTokens) override public view returns(uint256){
         return _balances[ownerTokens];
     }
-    function allowance(address ownerTokens, address spender) public view returns(uint256){
+    function allowance(address ownerTokens, address spender) override public view returns(uint256){
         return _allowances[ownerTokens][spender];
     }
 
-    function transfer(address to, uint256 value) public enoughTokens(msg.sender, value) returns(bool){
+    function transfer(address to, uint256 value) override public enoughTokens(msg.sender, value) returns(bool){
         _balances[msg.sender] -= value;
         _balances[to] += value;
         emit Transfer(msg.sender, to, value);
         return true;
     }
 
-    function transferFrom(address from, address to, uint256 value) public enoughTokens(msg.sender, value)  returns(bool){
+    function transferFrom(address from, address to, uint256 value) override public enoughTokens(msg.sender, value)  returns(bool){
         _balances[from] -= value;
         _balances[to] += value;
         emit Transfer(from, to, value);
         return true;
     }
 
-    function approve(address spender, uint256 value) public returns(bool){
+    function approve(address spender, uint256 value) override public returns(bool){
         _allowances[msg.sender][spender] = value;
         emit Approval(msg.sender, spender, value);
         return true;
     }
 
-    function burn(address account, uint256 amount) public onlyOwner{
+    function burn(address account, uint256 amount) override public onlyOwner{
         _balances[account] -= amount;
         _totalTokens -= amount;
     }
 
-    function mint(address account, uint256 amount) public onlyOwner{
+    function mint(address account, uint256 amount) override public onlyOwner{
         _balances[account] += amount;
         _totalTokens += amount;
     }
